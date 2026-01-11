608ee06670fa98bb575eb6b15bc2e4c2c58cf30e
Jorge Miranda <jmiranda32691@gmail.com>
2026-01-11 17:07:25 -0600

    accum and guard done

--- patterns/accumulator.js
+++ patterns/accumulator.js
@@ -22,6 +22,13 @@ export function sumToN(n) {
  * @returns `1` if n is 0
  */
 export function factorial(n) {
+  if (typeof n !== 'number') return NaN;
+  else if (n < 0) return undefined;
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial
   // TODO
 }
 
@@ -32,6 +39,12 @@ export function factorial(n) {
  * @returns `[]` if n is 0 or negative
  */
 export function buildNArray(n) {
+  if (typeof n !== 'number') return null;
+  const builtArr = []
+  for (let i = 1; i <= n; i++) {
+    builtArr.push(i)
+  }
+  return builtArr
   // TODO
 }
 
@@ -41,14 +54,31 @@ export function buildNArray(n) {
  */
 export function getLongestString(strings) {
   // TODO
+  let winner = strings[0]
+  for (let i = 1; i < strings.length; i++) {
+    if (strings[i].length > winner.length) {
+      winner = strings[i];
+    }
+  }
+  return winner
 }
-
+// console.log(getLongestString(['hello', 'goodbye', 'yessssssssssssssssssssssss', 'nooooooooooooo', 'okay']))
 /**
  * @param {boolean[]} attendance - `true` means a student is present, `false` means a student is absent
  * @returns {number} the number of students present
  */
 export function countPresent(attendance) {
   // TODO
+  let presentCount = 0
+  for (let i = 0; i < attendance.length; i++) {
+    if (attendance[i] === true) {
+      presentCount += 1;
+    }
+  }
+  return presentCount;
+
+  //using .reduce
+  //return attendance.reduce((count, isPresent) => count + (isPresent ? 1:0), 0)
 }
 
 /**
@@ -62,5 +92,18 @@ export function countPresent(attendance) {
  * @returns `null` if `dna` is not a string
  */
 export function complementDNA(dna) {
-  // TODO
+  if (typeof dna !== 'string') return null; //handles the string assertion
+  const complementObject = { // built a key:value to pass through reduce
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C'
+  };
+  return dna.split('').reduce((newStr, letter2check) => { // converts the dna string into an array with .split so we can use reduce method. 
+    //newStr is the accumulator which is starting off as an empty string since I set the last param of reduce to ''.
+    // letter2check is the starting position - index[0]- of the array because I did not supply an specific index to start on in the following optional param. 
+
+    return newStr + complementObject[letter2check] // this is what I want reduce to do everytime it iterates through the array.
+    //it's essentially using the object/library i created to make a new string with the values of each key that gets passed through. 
+  }, '') //this is the initial value of newStr. Since I was building a string, I just assigned it to equal an empty string. 
 }
--- patterns/guard.js
+++ patterns/guard.js
@@ -19,7 +19,13 @@ export function multiply(num1, num2) {
  */
 export function vote(age) {
   // TODO
+  if (typeof age !== 'number'){
+    return 'Age not valid.';
+  }
+  let text = (age < 18) ? 'You must be 18 or older to vote.' : 'Who would you like to vote for?';
+  return text
 }
+
 
 /**
  * @param {string} str1
@@ -28,5 +34,10 @@ export function vote(age) {
  * @returns `undefined` if either argument is not a string
  */
 export function concatenateStrings(str1, str2) {
+  if (typeof str1 !== 'string' || typeof str2 !== 'string'){
+    return undefined
+  }
+  return str1 + str2
   // TODO
 }
+
